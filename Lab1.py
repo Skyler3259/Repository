a26138a3c35f8bcfa682ff17a7eda5952a4f22b8
Skyler3259 <54539726+Skyler3259@users.noreply.github.com>
2019-09-06 07:52:13 -0500

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,31 @@
+# Lab_1
+# Name
+print("Skyler Williams")
+#Equation
+print("What is the temperature? in Celecius please.")
+C = int(input())
+
+result = ((C*1.8) + 32)
+#F = C * 1.8 + 32
+print(result)
+
+# Turtle Time
+
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("Black")
+screen.title("Super pentagon")
+
+drawing_turtle = turtle.Turtle()
+drawing_turtle.pencolor("Red")
+drawing_turtle.forward(100)
+drawing_turtle.left(72)
+drawing_turtle.forward(100)
+drawing_turtle.left(72)
+drawing_turtle.forward(100)
+drawing_turtle.left(72)
+drawing_turtle.forward(100)
+drawing_turtle.left(72)
+drawing_turtle.forward(100)
+turtle.done()
