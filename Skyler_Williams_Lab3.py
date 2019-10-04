f90f685b4d1aab3e06f314215b42e9cc9260217e
Skyler3259 <54539726+Skyler3259@users.noreply.github.com>
2019-10-04 09:50:54 -0400

    Add files via upload

--- Skyler_Williams_Lab3.py
+++ Skyler_Williams_Lab3.py
@@ -2,6 +2,9 @@ speedlimit = int(input("What is the speed limit?  "))
 
 carspeed = int(input("What is your speed?  "))
 
-x = (carspeed - speedlimit)/5
+x = (carspeed - speedlimit)//5
 
-print(x)
+print(x) 
+print("Demerit points")
+
+if x >= 12: print("License suspended.")
