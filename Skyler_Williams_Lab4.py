7346d50cdeb878160a5b4db3e65c3fa9661d0b97
Skyler3259 <54539726+Skyler3259@users.noreply.github.com>
2019-11-01 09:37:30 -0400

    Add files via upload

--- Skyler_Williams_Lab4.py
+++ Skyler_Williams_Lab4.py
@@ -1,12 +1,13 @@
+Shift = int(input("How many letters do you want to shift? "))
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 Words = ['Just', 'random', 'text']
 shift = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
 encryption = []
 
+#'c' = (((ord('z') + 3 ) - 97 ) %26 ) + 97
+#'c' = (((ord('letter') + shift ) - 97 ) %26 ) + 97
 
-
-
-
+shift = int(input("How many letters do you want to shift? "))
 
 
 
